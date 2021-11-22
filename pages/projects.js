737c92eeb302b623ddf8aae03cd06fc4cde21e93
MeherDeepak-2005 <68017128+MeherDeepak-2005@users.noreply.github.com>
2021-11-22 12:52:38 +0530

    Update projects.js

--- pages/projects.js
+++ pages/projects.js
@@ -1,34 +1,43 @@
 import {Image} from '@chakra-ui/react';
-import { Text, Heading, Box, Flex, VStack, Input,HStack } from '@chakra-ui/react';
+import { Text, Box, Flex, VStack, Input,HStack } from '@chakra-ui/react';
 import { SearchIcon } from "@chakra-ui/icons";
 import { useEffect, useState } from 'react';
-import { collection, getDocs,onSnapshot,where,query } from '@firebase/firestore';
+import { collection, getDocs,onSnapshot,where,query, getDoc } from '@firebase/firestore';
 import { db } from '../services/firebase';
 import Link from 'next/link'
 
 
 function Projects({ fetchedProjects }) {
   const [value, setValue] = useState('');
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState();
   const [searchStatus, setStatus] = useState(false);
-  const [projects, setProjects] = useState(JSON.parse(fetchedProjects));
+  const [projects, setProjects] = useState([]);
+  const [allowSearchDisplay, setAllowSearchDisplay] = useState(false);
 
 
-  const searchFunction = () => {
-    onSnapshot(query(collection(db, 'projects'), where('projectName', '>=', search)), snapshot => {
-      setProjects(snapshot.docs)
-    })
-  }
+  useEffect(() => {
+    if (!searchStatus) {
+      setProjects(JSON.parse(fetchedProjects))
+    }
+    else {
+      onSnapshot(query(collection(db, 'projects'), where('projectName', '>=', search)), snapshot => {
+        setProjects(snapshot.docs)
+        setAllowSearchDisplay(true)
+      })
+    }
+    }, [searchStatus,search,fetchedProjects]
+  )
+
+
 
 
   const handleSearch = (e) => {
     setValue(e.target.value)
     setSearch(e.target.value)
-    searchFunction();
     setStatus(true)
   }
 
-  if (searchStatus) {
+  if (allowSearchDisplay) {
     return (
       <>
         <div style={{marginTop:'4rem'}}>
@@ -41,12 +50,12 @@ function Projects({ fetchedProjects }) {
         </div>
         <Flex direction='row' flexWrap='wrap'>
           {projects.map(project => (
-            <Link key={project.data().id} passHref href={project.data().file}>
+            <Link key={project.data().file} passHref href={project.data().file || 'https://google.com/maps'}>
               <Box overflow='hidden' bg='gray.50' w='40rem' m='1rem' py={10} px={10}>
-                <Image w='40rem' h='20rem' transition='all .5s ease-in' _hover={{ transform: 'scale(1.1)' }} borderRadius={10} src={project.data().image} alt="VHR Infra's Project" />
+                <Image w='40rem' h='20rem' transition='all .5s ease-in' _hover={{ transform: 'scale(1.1)' }} borderRadius={10} src={project.data().image || 'images.unsplash.com'} alt="VHR Infra's Project" />
                 <VStack>
-                  <Text fontSize='3xl' py={6}>{project.projectName}</Text>
-                  <Link passHref href={project.data().projectMaps || "https://maps.google.com"}>
+                  <Text fontSize='3xl' py={6}>{project.data().projectName}</Text>
+                  <Link passHref href={project.projectMaps || "https://maps.google.com"}>
                     <Flex _hover={{ textDecoration: 'underline' }} alignItems='center' py={0}>
                       <svg xmlns="http://www.w3.org/2000/svg" height="10px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0z" fill="none" /><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" /></svg>
                       <Text cursor='pointer' fontSize='xl'>{project.data().projectLocation}</Text>
@@ -73,7 +82,7 @@ function Projects({ fetchedProjects }) {
         </div>
         <Flex direction='row' flexWrap='wrap'>
           {projects.map(project => (
-            <Link key={project.id} passHref href={project.file || 'http://Googleapis.com'}>
+            <Link key={project.file} passHref href={project.file}>
               <Box overflow='hidden' bg='gray.50' w='40rem' m='1rem' py={10} px={10}>
                 <Image w='40rem' h='20rem' transition='all .5s ease-in' _hover={{ transform: 'scale(1.1)' }} borderRadius={10} src={project.image} alt="VHR Infra's Project" />
                 <VStack>
